18e5618d858f7c8a6e0b79a657702a3247d37c50
Zachary Briggs <zacbriggs@outlook.com>
2020-08-11 09:41:07 +0100

    aws

--- src/components/app/AppContainer.jsx
+++ src/components/app/AppContainer.jsx
@@ -47,7 +47,7 @@ class AppContainer extends Component {
 
   connectWebSocket() {
     if (window.WebSocket) {
-      const conn = new WebSocket('ws://localhost:8080/ws');
+      const conn = new WebSocket('ws://ec2-3-10-221-71.eu-west-2.compute.amazonaws.com:8080/ws');
       this.setState({ conn });
       // Reader for incoming messages
       conn.onmessage = this.handleMessage;
@@ -107,7 +107,7 @@ class AppContainer extends Component {
         const form = document.createElement('form');
         document.body.appendChild(form);
         form.method = 'post';
-        form.action = 'http://localhost:8080/login';
+        form.action = 'http://ec2-3-10-221-71.eu-west-2.compute.amazonaws.com:8080/login';
         const sessionInput = document.createElement('input');
         sessionInput.type = 'hidden';
         sessionInput.name = 'session';
